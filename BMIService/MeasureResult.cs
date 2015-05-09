df4ebafff5143be977c6d739fd7640d106c36f19
小朱 (Jeff Chu) <regionbbs@gmail.com>
2015-05-09 13:50:21 +0800

    Missed commit

---
+++ BMIService/MeasureResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIService
+{
+    public enum MeasureResult
+    {
+        Less,
+        Normal,
+        More
+    }
+}
---
+++ BMIService/MenBodyMeasureIndex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIService
+{
+    public class MenBodyMeasureIndex : IBodyMeasureIndex
+    {
+        public MeasureResult Calculate(double Height, double Weight, out double BMI)
+        {
+            BMI = Weight / Math.Pow(Height, 2);
+
+            if (BMI < 20)
+                return MeasureResult.Less;
+            else if (BMI > 25)
+                return MeasureResult.More;
+            else
+                return MeasureResult.Normal;
+        }
+    }
+}
